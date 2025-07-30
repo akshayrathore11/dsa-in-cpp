796a9fb3fd47380ff4f347d18b0f08a1f1b41a53
Akshay Rathore <akshayrathore9010@gmail.com>
2025-07-30 14:59:46 +0530

    add reverse dll

---
+++ 06_Linked-List/09_reverse-DLL.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node *back;
+
+    Node(int data1, Node *next1, Node *back1)
+    {
+        data = data1;
+        next = next1;
+        back = back1;
+    }
+
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+        back = nullptr;
+    }
+};
+
+Node *convertArr2DLL(vector<int> arr)
+{
+    if (arr.empty())
+        return nullptr;
+
+    Node *head = new Node(arr[0]);
+    Node *prev = head;
+
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[i], nullptr, prev);
+        prev->next = temp;
+        prev = temp;
+    }
+
+    return head;
+}
+
+void print(Node *head)
+{
+    while (head != nullptr)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+Node *reverseDLL(Node *head)
+{
+    if (head == NULL || head->next == NULL)
+        return head;
+
+    Node *prev = NULL;
+    Node *current = head;
+
+    while (current != NULL)
+    {
+        prev = current->back;
+        current->back = current->next;
+        current->next = prev;
+        current = current->back;
+    }
+
+    return prev; 
+}
+
+int main()
+{
+    vector<int> arr = {12, 5, 8, 7, 4};
+    Node *head = convertArr2DLL(arr);
+
+    cout << "Doubly Linked List Initially:\n";
+    print(head);
+
+    head = reverseDLL(head);
+
+    cout << "Doubly Linked List After Reversing:\n";
+    print(head);
+
+    return 0;
+}
